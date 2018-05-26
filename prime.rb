4b10bae8a746414a36af2f96ec371b5c28dfa5ea
Mohammed Noori <kmanoorib@gmail.com>
2018-05-26 02:32:39 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,6 +3,17 @@ def prime?(argu)
   
   prime_numbers = [2,3,7]
   
+      for(var i = 2; i < value; i++) {
+        if(value % i === 0) {
+            return false;
+        }
+    }
+    return value > 1;
+  
+  count = 2
+  
+  for count = 2
+  
   if argu == 2 || argu == 3 || argu == 11 || argu == 7
     true
   elsif argu % 2 == 0 || argu % 3 == 0 || argu % 7 == 0
