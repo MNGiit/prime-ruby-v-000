7ae81a6947abe5e9ba6f978b4ef7c17f957b1af9
Mohammed Noori <kmanoorib@gmail.com>
2018-05-26 02:25:55 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(argu)
+  
+  prime_numbers = [2,3,7]
+  
+  if argu == 2 || argu == 3 || argu == 11 || argu == 7
+    true
+  elsif argu % 2 == 0 || argu % 3 == 0 || argu % 7 == 0
+    false
+  elsif argu < 0 || argu == 1
+    false
+  else
+    true
+  end
+end
